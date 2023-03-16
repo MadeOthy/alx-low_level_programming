613b0741d3179f36958fd2a984bd32ba9262b1cc
MadeOthy <othmane.barakat23@gmail.com>
2023-03-16 05:27:12 -0700

    4 puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int mani(void)
+{
+	puts("\"Programing is like building a multitingual puzzle");
+	return (0);
+}
